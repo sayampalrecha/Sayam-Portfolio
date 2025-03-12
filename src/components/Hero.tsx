f2705379abb874938ef5a1a62984988d88d145af
Sayam Palrecha <104487341+sayampalrecha@users.noreply.github.com>
2025-03-12 07:27:41 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -46,6 +46,12 @@ const Hero: React.FC<HeroProps> = ({ className }) => {
           >
             Get In Touch
           </a>
+          <a 
+            href="https://drive.google.com/file/d/1UtZDID3BzuC8SsUWzM2Dxc9on4_dr9Jo/view?usp=sharing" 
+            className="bg-secondary text-primary px-8 py-3.5 rounded-lg font-medium transition-all hover:translate-y-[-1px] hover:shadow-sm active:translate-y-[1px]"
+          >
+            Resume
+          </a>
         </div>
       </div>
       
