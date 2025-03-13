d14a27784157ac5834e497621ea00d0251c8e8ef
Sayam Palrecha <104487341+sayampalrecha@users.noreply.github.com>
2025-03-13 09:35:34 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -28,7 +28,7 @@ const projectsData = [
     title: 'Medical Data Visualization',
     description: 'Data visualization of medical data using static and dynamic plots',
     imageSrc: 'https://images.unsplash.com/photo-1584291527935-456e8e2dd734?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D,
-    tags: ['Data Visualization', 'Python', 'Plotly','Matplotlib & Seaborn'],
+    tags: ['Python', 'Plotly','Matplotlib & Seaborn'],
     link: '#',
   },
   {
